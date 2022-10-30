6aebeb584a6f290d3ae82272b26a1828498cb918
Aerol <dc.chan@me.com>
2022-10-30 11:44:53 -0500

    Updated the deals page to include more content and organized the layout.

--- src/Deals.jsx
+++ src/Deals.jsx
@@ -6,7 +6,13 @@ const Deals = () => {
       <div className = 'deals-policy-title'>
           <h1 className='deals-title'>Policy</h1>
       </div>
-      <p className='deals-paragraph'>This is cool <br></br> page!!!!</p>
+      <p className='deals-paragraph'>
+        1. No refunds are allowed.
+        <br></br>
+        2. We are not responsible for any undesirable results from purchasing at a non-retailer store.
+        <br></br>
+        3. Any listings found by our algorithim may not necessarily represent our views.
+      </p>
     </div>
   )
 }
