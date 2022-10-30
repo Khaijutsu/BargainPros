2649b258ab388e180403139b976f206b47d22302
Aerol <dc.chan@me.com>
2022-10-30 11:14:40 -0500

    Made header and made test paragrpah for deals section

--- src/Deals.jsx
+++ src/Deals.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Deals = () => {
   return (
-    <div>Deals</div>
+    <div className = 'deals-content'>
+      <div className = 'deals-policy-title'>
+          <h1 className='deals-title'>Policy</h1>
+      </div>
+      <p className='deals-paragraph'>This is cool <br></br> page!!!!</p>
+    </div>
   )
 }
 
